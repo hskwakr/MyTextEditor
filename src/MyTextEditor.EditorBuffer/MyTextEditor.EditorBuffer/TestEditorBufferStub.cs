a42f52d92d14484929b2dbc42130711dacb5b47f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-01-27 10:54:03 +0900

    Create TestEditorBufferStub.cs

---
+++ src/MyTextEditor.EditorBuffer/MyTextEditor.EditorBuffer/TestEditorBufferStub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MyTextEditor.EditorBuffer
+{
+    public abstract class TestEditorBufferStub
+    {
+        public readonly string TextSequence;
+        public TestEditorBufferStub()
+        {
+            TextSequence =
+                "Hello, nice to meet you.";
+        }
+    }
+}
