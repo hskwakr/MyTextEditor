4e71a2ba03adea3fe032fcd5352d2d22c6bf85ac
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-01-31 14:52:43 +0900

    Remove unnecessary usings and white spaces

--- src/MyTextEditor.EditorBuffer/PieceType.cs
+++ src/MyTextEditor.EditorBuffer/PieceType.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using Xunit;
-
-namespace MyTextEditor.EditorBuffer
+﻿namespace MyTextEditor.EditorBuffer
 {
-
     public enum PieceType
     {
         OriginalText,
